4e879e1caa466f3d0e8105ccd2be589893393cce
Shubh Jaiswal <shubhcodez@gmail.com>
2023-10-06 23:27:04 +0530

    addition feature implemented

---
+++ cpp/addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Step 1: Accepting Input
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Step 2: Performing Addition
+    int sum = num1 + num2;
+
+    // Step 3: Displaying the Result
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
