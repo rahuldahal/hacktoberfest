5103a3b57a821c1cf2dd1af538cbab3b05073206
Princy Ballabh <princyballabh13@gmail.com>
2023-10-08 02:12:39 +0530

    Added the Addition Feature

---
+++ c/Addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+float add()
+{
+    int a,b;
+    printf("enter the first number")
+    scanf("%f",&a);
+    printf("enter the second number")
+    scanf("%f",&b);
+    sum=a+b;
+    printf("%f",a," + ","%f",b," = %f",sum);
+}
