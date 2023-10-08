d05cecdf5e29c624f00102b22e9c2d2ce5ccb1ea
Princy Ballabh <princyballabh13@gmail.com>
2023-10-08 22:00:48 +0530

    Updated Addition Feature

--- c/Addition.c
+++ c/Addition.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-float add()
+
+int main(int argc, char *argv[])
 {
-    int a,b;
-    printf("enter the first number")
-    scanf("%f",&a);
-    printf("enter the second number")
-    scanf("%f",&b);
-    sum=a+b;
-    printf("%f",a," + ","%f",b," = %f",sum);
+	int a,b,sum;
+	if(argc!=3)
+	{
+		printf("please use \"prg_name value1 value2 \"\n");
+		return -1;
+	}
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a+b;
+	
+	printf("Sum of %d, %d is: %d\n",a,b,sum);
+	
+	return 0;
 }
