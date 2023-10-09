806f34fb6e8de7b179dcea2639f48160b5fa157c
Milan Bhandari <103623160+milanbhandari047@users.noreply.github.com>
2023-10-09 07:21:37 +0545

    multiplication feature added

---
+++ cpp/multip;ication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Step 1: Accepting Input
+    float num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Step 2: Performing multiplication
+    float mul = num1 * num2;
+
+    // Step 3: Displaying the Result
+    cout << "The multiplication  of two numbers  is: " << mul << endl;
+
+    return 0;
+}
