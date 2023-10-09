c608dba67e390e112be44b6dc1f9dfbcf170c9a3
milanbhandari047 <milanbhandari047@gmail.com>
2023-10-09 06:29:52 +0545

    subtraction feature added

---
+++ cpp/subtraction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Step 1: Accepting Input
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Step 2: Performing subtraction
+    int sub = num1 - num2;
+
+    // Step 3: Displaying the Result
+    cout << "The sub of two number  is: " << sub << endl;
+
+    return 0;
+}
