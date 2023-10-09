c2d477356669cf804d16f8b6cb4fa11fada43b0a
Milan Bhandari <103623160+milanbhandari047@users.noreply.github.com>
2023-10-09 07:10:10 +0545

    division feature added

---
+++ cpp/division.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Step 1: Accepting Input
+    float num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    // Step 2: Performing division
+    float div = num1 / num2;
+
+    // Step 3: Displaying the Result
+    cout << "The division of two number is: " << div << endl;
+
+    return 0;
+}
